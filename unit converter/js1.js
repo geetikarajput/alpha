8bc46a49de84192c4e291fcf2efac946ae255b3b
geetika <geetikapundir23@gmail.com>
2024-06-15 22:55:24 +0530

    unit converter project

---
+++ unit converter/js1.js
@@ -0,0 +1,20 @@
+
+document.getElementById('main').addEventListener("mouseover",function(){this.style.backgroundColor='#66FCF1';})
+document.getElementById('main').addEventListener("mouseout",function(){this.style.backgroundColor='#45A29E';})
+var feet = document.getElementById('feet');
+var inch = document.getElementById('inch');
+
+feet.addEventListener('input',function(){
+    let f = this.value;
+    let i = f*12;
+    inch.value=i;
+})
+
+inch.addEventListener('input',function(){
+    let i = this.value;
+    let f = 1/12;
+    if(!Number.isInteger(f))
+        {
+            f = f.toFixed(2);
+        }
+})
